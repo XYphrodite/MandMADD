2fcf973ed013019a73d4769b6d29c5362cbfc95f
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-05-24 08:31:08 +0300

    six

--- 6.py
+++ 6.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+import numpy
 
 lamb_1 = 0.4211  #
 lamb_2 = 0.7371  #
@@ -8,13 +9,14 @@ sigma_Sqr_0 = 3  #
 alfa_0 = 0.15  #
 
 epsilon = 0.004  #
-A1 = 1.7  #
-A2 = 0.32  #
+A1 = 1  # 1.7  #
+A2 = 1  # 0.32  #
 Ns = 15  #
 
 lamb_3 = 0
-S = 6  # или 5 или 6
+S = 6  # 5 или 6
 N = 200
+
 
 def rand(l1, l2):
     l3 = (l1 * l2 * 1e8) // 100
@@ -23,124 +25,112 @@ def rand(l1, l2):
     return l3
 
 
-# Find M-middle
-def Middle(z):
-    sum = 0
-    for i in z:
-        sum += i
-    return sum / len(z)
+Middle = lambda arr: sum(arr) / len(arr)
 
 
-# Find middle-square
-def Square(z):
-    sum1 = 0
-    for i in z:
-        sum1 += i - Middle(z) ** 2
-    return sum1 / len(z)
+def Square(arr):
+    _sum = 0
+    for i in arr:
+        _sum += (i - Middle(arr)) ** 2
+    return _sum / len(arr)
 
 
-# Gen_random_process
 def Process(k):
-    l = k
+    i = k
     sum2 = 0
-    while l < k + Ns:
-        sum2 += z[l] * ((sigma_Sqr_0 / (alfa_0 * A2 * Square(z))) ** 0.5) * A1 * math.exp(- alfa_0 * A2 * (l - k))
-        l += 1
+    while i < k + Ns:
+        sum2 += rand_arr[i] * math.pow((sigma_Sqr_0 / (alfa_0 * A2 * Square(rand_arr))), 0.5) * A1 * math.exp(
+            -A2 * alfa_0 * (i - k))
+        i += 1
     return (sum2 / Ns) + M_0
 
 
-# Korrelation
-def Korrel(S):
+def Korrel(z, S):
     p = 0
     sum3 = 0
-    for p in range(len(f) - S):
-        sum3 += (f[p] - Middle(f)) * (f[p + S] - Middle(f))
-        # p+=1
+    for p in range(len(z) - S):
+        sum3 += (z[p] - Middle(z)) * (z[p + S] - Middle(z))
+        p += 1
     return sum3 / p
 
+def GetRandomArr():
+    arr= []
+    i = 0
+    while i < N - Ns:
+        arr.append(Process(i))
+        i += 1
+    return arr
 
-z = []
-K = []
-Kp = []
+rand_arr = []
+Corr_arr = []
+Corr_arr_ = []
 alfa = []
-f = []
+randProcessArr = []
 
-m = 0
-while m < N - 1:
-    z.insert(m + 1, lamb_1 - 0.5)  # only var_16-19
-    lamb_3 = rand(lamb_1, lamb_2)  # only var_16-19
-    lamb_1 = lamb_2  # only var_16-19
-    lamb_2 = lamb_3  # only var_16-19
+oldA = 0
+aldM = 0
 
-    #  z.insert(m+1, rand(z[m]))  #only var_1-15 and var_21-50
-    m += 1
-m = 0
+# random
+for i in range(N - 1):
+    rand_arr.append(lamb_1 - 0.5)
+    lamb_3 = rand(lamb_1, lamb_2)
+    lamb_1 = lamb_2
+    lamb_2 = lamb_3
 
-print("Среднее ряда=", Middle(z))
-print("Мат.ожидание ряда=", Square(z))
+i = 0
+# random f
+randProcessArr = GetRandomArr()
 
-# Generate random function f[i]
-while m < N - Ns:
-    f.insert(m, Process(m))
-    m += 1
-m = 0
+if  abs(Square(randProcessArr) - sigma_Sqr_0) > 0.15:
+    randProcessArr.clear()
+    A2-=0.01
+    randProcessArr = GetRandomArr()
 
+
+i = 0
 # Korrelate teoretic f[i]
-while m < S:
-    K.insert(m, Korrel(m))
-    m += 1
+while i < S:
+    Corr_arr.insert(i, Korrel(randProcessArr, i))
+    i += 1
 
 alfa_f = 0
-m = 0
+i = 0
 Fi = 10000
-l = 0
 
-print("Среднее случ. функции=", Middle(f))
-print("Мат.ожидание функции=", Square(f))
-
-## Korrelate practic and aproximate K teoretic
 while Fi > epsilon:
     Fi = 0
-    Kp.clear()
-    while m < S:
-        Fi += (Square(f) * math.exp(-alfa_f * m) - K[m]) ** 2
-        Kp.insert(m, Square(f) * math.exp(-alfa_f * m))
-        m += 1
-    l += 1
+    Corr_arr_.clear()
+    while i < S:
+        Fi += (Square(randProcessArr) * math.exp(-alfa_f * i) - Corr_arr[i]) ** 2
+        Corr_arr_.insert(i, Square(randProcessArr) * math.exp(-alfa_f * i))
+        i += 1
     alfa_f += 0.01
     Fi = Fi / S
-    #  print(Fi)
-    m = 0
+    i = 0
 
 alfa_f -= 0.01
-m = 0
 
-print("Параметр аппроксимации функции=", alfa_f)
+print("Среднее ряда=", Middle(rand_arr))
+print("Мат.ожидание ряда=", Square(rand_arr))
+print("Среднее случ. функции=", Middle(randProcessArr))
+print("Мат.ожидание функции=", Square(randProcessArr))
+print("Параметр аппроксимации функции = ", alfa_f)
 
 # graph
-TN = []
-TS = []
-j = 0
-while j < N - Ns:
-    TN.append(j)
-    j += 1
-
-j = 0
-while j < S:
-    TS.append(j)
-    j += 1
+TN = numpy.arange(0, N - Ns)
+TS = numpy.arange(0, S)
 
 fig, (ax1, ax2) = plt.subplots(2, 1)
 
 ax1.set_ylabel('Знач. случ. процесса')
 ax1.set_xlabel('N')
 
-ax1.plot(TN, f)
+ax1.plot(TN, randProcessArr)
 
 ax2.set_ylabel('Знач. коррел. функции')
 ax2.set_xlabel('S')
 
-ax2.plot(TS, K, color="blue", marker=".")
-ax2.plot(TS, Kp, color="red", marker=".")
+ax2.plot(TS, Corr_arr, color="blue", marker=".")
+ax2.plot(TS, Corr_arr_, color="red", marker=".")
 
 plt.show()
